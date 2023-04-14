2de2046649657e228b86c15cd52d2229ba1e4412
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-14 22:01:56 +0530

    feat: initial commit

---
+++ src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect } from '../helpers/type-utils';
+
+const returnWhatIPassIn = <T>(t: T) => {
+	return t;
+};
+
+const one = returnWhatIPassIn(1);
+const matt = returnWhatIPassIn('matt');
+
+type ReturnWhatIPassIn<T> = T;
+
+type one = ReturnWhatIPassIn<1>;
+
+type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, 'matt'>>];
